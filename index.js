4d5a68a558859c4379d05b34b1bf1e1b41610ef2
geodude3 <77406516+geodude3@users.noreply.github.com>
2023-01-05 12:34:24 -0800

    Second commit: Added support for button in index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    const btn = document.querySelector("button");
+
+    btn.addEventListener('click',()=>{
+        document.body.style.backgroundColor = "red";
+    });
+
+});
