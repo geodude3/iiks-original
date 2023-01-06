9b24387c55678e771452bb4789fd5a35938259ea
geodude3 <77406516+geodude3@users.noreply.github.com>
2023-01-05 17:59:29 -0800

    created new exp2 branch and added colour changing feature on button

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
     btn.addEventListener('click',()=>{
 
         let colour = window.getComputedStyle( document.body ,null).getPropertyValue('background-color');
-        
+
         if (colour == 'rgb(255, 0, 0)'){
             document.body.style.backgroundColor = "yellow";
+            document.querySelector("button").backgroundColor = "red";
         }
         else{
             document.body.style.backgroundColor = "red";
+            document.querySelector("button").backgroundColor = "red";
         }
     });
 
