6ceb0a7fca78d228e07bb2446a00548bb0c5d878
geodude3 <77406516+geodude3@users.noreply.github.com>
2023-01-05 17:54:30 -0800

    small edit

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const btn = document.querySelector("button");
     
     btn.addEventListener('click',()=>{
+
         let colour = window.getComputedStyle( document.body ,null).getPropertyValue('background-color');
+        
         if (colour == 'rgb(255, 0, 0)'){
             document.body.style.backgroundColor = "yellow";
         }
